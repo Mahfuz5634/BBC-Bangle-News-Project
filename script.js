b3324704672b99395c3ee07f5c13b056837a3a73
Mahfuz <mahfuzru5634@gmail.com>
2025-09-06 07:53:02 +0600

    added auto shows main page

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ const loadcategories=() => {
    url="https://news-api-fs.vercel.app/api/categories";
    fetch(url)
    .then(res=>res.json())
-   .then(data=>displaycategories(data.categories))
+   .then(data=>displaycategories(data.categories));
+  
 }
 const displaycategories=(data)=>{
     const nav=document.getElementById('navbar');
 
     for(const dt of data){
         const categories=document.createElement('li')
-        categories.innerHTML=`
+        if(dt.id==='main'){
+          categories.innerHTML=`
+         <li id="${dt.id}" class="border-b-3 border-red-700  cursor-pointer">${dt.title}</li>
+        `
+        }
+        else{
+         categories.innerHTML=`
          <li id="${dt.id}" class="hover:border-b-3 border-red-700  cursor-pointer">${dt.title}</li>
         `
+        }
+        
         nav.appendChild(categories);
         
     }
@@ -66,6 +75,7 @@ const showImage =(articles)=>{
     }
 
 }
+displayNews('main');
 
 
 
